1b01efe76823fab6748990e651efe125687cf5fc
909185693 <909185693@qq.com>
2018-05-05 11:26:33 +0800

    Initialization Projects

---
+++ Source/ToAmuse/Public/GameMode/TGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOAMUSE_API ATGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ToAmuse/ToAmuse.Build.cs
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class ToAmuse : ModuleRules
+{
+	public ToAmuse(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "UMG",
+            "Networking",
+            "Engine",
+            "InputCore"
+        });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {
+            "OnlineSubsystem"
+        });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
